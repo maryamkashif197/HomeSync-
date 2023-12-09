d9143155426a8d7b8a6bc4b25360ed396392d981
D-Ace <ahmedmouad578@gmail.com>
2023-12-09 16:09:09 +0200

    update register

--- eLibrary/eLibrary/register.aspx.cs
+++ eLibrary/eLibrary/register.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,7 @@ namespace eLibrary
 {
     public partial class register : System.Web.UI.Page
     {
+
         string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -23,28 +25,41 @@ namespace eLibrary
             
             try
             {
-                SqlConnection con = new SqlConnection(strcon);
-                if(con.State == ConnectionState.Closed)
+                using (SqlConnection con = new SqlConnection(strcon))
                 {
+                    SqlCommand cmd = new SqlCommand("UserRegister", con);
+
+                    cmd.Parameters.Add("@usertype", SqlDbType.VarChar).Value = usertype.Text;
+                    cmd.Parameters.Add("@email", SqlDbType.VarChar).Value = email.Text;
+                    cmd.Parameters.Add("@first_name", SqlDbType.VarChar).Value = usertype.Text;
+                    cmd.Parameters.Add("@last_name", SqlDbType.VarChar).Value = lastname.Text;
+                    cmd.Parameters.Add("@birth_date", SqlDbType.Date).Value = brithdate.Text;
+                    cmd.Parameters.Add("@password", SqlDbType.VarChar).Value = password.Text;
+
+                    SqlParameter userId = new SqlParameter("@user_id", SqlDbType.Int);
+                    userId.Direction = ParameterDirection.Output;
+
+                    cmd.Parameters.Add(userId);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
                     con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    if(userId.Value.ToString() != "")
+                    {
+
+                        Response.Write("thanks for register, your id is: " + userId.Value.ToString());
+
+                    }
+                    else
+                    {
+                        Response.Write("something went wrong");
+                    }
                 }
-
-                SqlCommand cmd = new SqlCommand("UserRegister", con);
                 
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@usertype", usertype.Text);
-                cmd.Parameters.AddWithValue("@email", email.Text);
-                cmd.Parameters.AddWithValue("@first_name", firstname.Text);
-                cmd.Parameters.AddWithValue("@last_name", lastname.Text);
-                cmd.Parameters.AddWithValue("@birth_date", brithdate.Text);
-                cmd.Parameters.AddWithValue("@password", password.Text);
-                SqlParameter outputParameter = new SqlParameter("@user_id", SqlDbType.Int);
-                outputParameter.Direction = ParameterDirection.Output;
-                cmd.Parameters.Add(outputParameter);
 
-                cmd.ExecuteNonQuery();
-                con.Close();
-                Response.Write("thanks for register, your id is: "+outputParameter.Value+"");
+               
             }
             catch (Exception ex)
             {
